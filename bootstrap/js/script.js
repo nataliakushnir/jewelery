649bd2d1380dc450282b35be39ca42481ab912d7
nataliakushnir <natalia.l.kushnir@gmail.com>
2015-05-28 15:25:54 +0300

    added final version

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -1,10 +1,20 @@
-
-if (document.body.clientWidth > 991) {
-	imageHeight = getComputedStyle(document.getElementById('imageHeight')).height;
-	titleHeight = document.getElementById('titleHeight3');
-	titleHeight.style.height = imageHeight;
-
-	imageHeight = getComputedStyle(document.getElementById('imageHeight')).height;
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
+imageHeight = getComputedStyle(document.getElementById('imageHeight')).height;
 	titleHeight = document.getElementById('titleHeight');
 	titleHeight.style.height = imageHeight;
-}
+
+imageHeight = getComputedStyle(document.getElementById('imageHeight')).height;
+	titleHeight = document.getElementById('titleHeight1');
+	titleHeight.style.height = imageHeight;
+	
+	window.onresize = function() {
+    imageHeight = getComputedStyle(document.getElementById('imageHeight')).height;
+	titleHeight = document.getElementById('titleHeight');
+	titleHeight.style.height = imageHeight;
+
+imageHeight = getComputedStyle(document.getElementById('imageHeight')).height;
+	titleHeight = document.getElementById('titleHeight1');
+	titleHeight.style.height = imageHeight;
+};
--- css/script/script.js
+++ css/script/script.js
@@ -1,10 +1,10 @@
-/*imageHeight = getComputedStyle(document.getElementById('imageHeight')).height;
+imageHeight = getComputedStyle(document.getElementById('imageHeight')).height;
 titleHeight = document.getElementById('titleHeight1');
 titleHeight.style.height = imageHeight;
 
 imageHeight = getComputedStyle(document.getElementById('imageHeight')).height;
 titleHeight = document.getElementById('titleHeight');
-titleHeight.style.height = imageHeight;*/
+titleHeight.style.height = imageHeight;
 
 $(document).ready(function(){
   $('.bxslider').bxSlider();
